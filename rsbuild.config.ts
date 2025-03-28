157262b9833f357de6066d2da09029f6d25c72d6
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-03-28 08:54:14 +0100

    Update rsbuild.config.ts server configration

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [pluginReact(), pluginSass()],
+  server: {
+    port: 8080
+  },
+  html: {
+    title: 'React Image Cropper'
+  },
+  source: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 });
