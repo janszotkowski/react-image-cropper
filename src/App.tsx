9881ee194e7cc64290d062a0a36ea2dd3e71ed48
Jan Szotkowski <szotkowski.honz@seznam.cz>
2025-03-28 20:09:03 +0100

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 
 const App = (): React.ReactElement => {
     return (
-        <div className={'w-full h-screen flex flex-col items-center justify-center'}>
-            <h1>React, TypeScript, ESLint, Stylelint & RSBuild template.</h1>
-        </div>
+        <main className={'w-full h-screen flex flex-col items-center justify-center text-white'}>
+            <h1 className={'text-2xl mb-8'}>Image Editor</h1>
+        </main>
     );
 };
 
